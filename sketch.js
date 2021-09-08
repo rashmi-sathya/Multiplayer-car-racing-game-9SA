a918b26a16084c77479ce1944af14fe7a3ada286
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-09-08 14:39:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var database, gameState;
 var form, player, playerCount;
 var allPlayers, car1, car2,fuels, powerCoins;
 var cars = [];
-var fuelImage, powerCoinImage;
+var fuelImage,lifeImage , powerCoinImage;
 //BP
 function preload() {
   backgroundImage = loadImage("./assets/background.png");
